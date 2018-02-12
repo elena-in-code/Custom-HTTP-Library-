99e6481055d31a7b8dd85c98de6442d51272496c
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-02-12 12:26:40 +0100

    DELETE method

--- easyhttp.js
+++ easyhttp.js
@@ -45,3 +45,16 @@ easyHTTP.prototype.put = function(url, data, callback){
 }
 
 // Make an HTTP DELETE request
+easyHTTP.prototype.delete = function(url, callback) {
+    this.http.open("DELETE", url, true);
+
+    let self = this;
+    this.http.onload = function(){
+        if(self.http.status === 200){
+            callback(null, "Post Deleted. This post it is not longer available.");
+        }else {
+            callback("Error: " + self.http.status);
+        }
+    }
+    this.http.send();
+}
