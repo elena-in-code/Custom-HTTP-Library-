4152b208f2d9266a96ed23741e52acd132fb5f28
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-02-12 12:19:19 +0100

    PUT method

--- app.js
+++ app.js
@@ -24,11 +24,20 @@ const data = {
     body: "This is a custom post"
 };
 //Create post
-http.post("https://jsonplaceholder.typicode.com/posts", data, function(err, post) {
+/* http.post("https://jsonplaceholder.typicode.com/posts", data, function(err, post) {
     if(err){
         console.log(err);
     } else {
         console.log(post);
     }
 });
+ */
 
+ //Update post
+ http.put("https://jsonplaceholder.typicode.com/posts/1", data, function(err, post) {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
