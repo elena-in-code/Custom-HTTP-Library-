7c7807d3b5bbd67dcdba8793116fc02b0b739cfa
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-02-12 12:03:43 +0100

    POST  method

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const http = new easyHTTP;
 
-// Get Posts
-http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts){
+// GET Posts
+/* http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts){
     if(err){
         console.log(err);
     } else {
         console.log(posts);
     }
 });
+ */
+//GET single post
+/* http.get("https://jsonplaceholder.typicode.com/posts/1", function(err, post){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+}); */
+
+//Create data
+const data = {
+    title: "custom post",
+    body: "This is a custom post"
+};
+//Create post
+http.post("https://jsonplaceholder.typicode.com/posts", data, function(err, post) {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(post);
+    }
+});
+
